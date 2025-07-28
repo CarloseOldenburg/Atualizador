e78444223083a700abb609ee6c8beb2c526d23c8
Carlos Oldenburg <carlos.eoldenburg@gmail.com>
2025-07-28 20:28:37 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
 
-# Instalando deps de sistema e pip
 sudo apt update
 sudo apt install -y python3 python3-pip curl
 
-# Instala libs Python
-pip3 install --upgrade pip
-pip3 install --upgrade rich textual requests
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade rich textual requests
 
-# Baixa e executa app (sempre atual, sempre do GH)
 cd /tmp
 rm -f rollout_vsfood.py
 curl -sSL https://raw.githubusercontent.com/CarloseOldenburg/rollout-vsfood/refs/heads/main/rollout_vsfood.py -o rollout_vsfood.py
